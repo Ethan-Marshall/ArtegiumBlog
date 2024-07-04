0f600e35d8d7eca66e24bbc557d21121d4f4183b
Ethan Marshall <ethan.n.marshall@protonmail.com>
2024-07-04 06:21:13 -0700

    Update AuthorLayout.tsx

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -38,6 +38,7 @@ export default function AuthorLayout({ children, content }: Props) {
               <SocialIcon kind="github" href={github} />
               <SocialIcon kind="linkedin" href={linkedin} />
               <SocialIcon kind="x" href={twitter} />
+              <SocialIcon kind="mastodon" href={mastodon} />
               <SocialIcon kind="youtube" href={youtube} />
             </div>
           </div>
