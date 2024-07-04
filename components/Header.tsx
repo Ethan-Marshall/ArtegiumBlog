0636c3dca10abda8f92bafba02974fb771e8b8c5
Ethan Marshall <ethan.n.marshall@protonmail.com>
2024-07-04 16:14:35 -0700

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,7 +13,15 @@ const Header = () => {
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
             <div className="mr-3">
-              <Image src={Logo.src} width={150} height={150} alt="Logo" />
+              {Logo && (
+                <Image
+                src={Logo}
+                alt="Logo"
+                width={150}
+                height={150}
+                quality={100}
+                />
+              )}
             </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
               <div className="hidden h-6 text-2xl font-semibold sm:block">
